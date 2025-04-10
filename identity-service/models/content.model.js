a83e98969fb2a4e72179c9e0d209351d877b1421
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2025-04-10 06:13:20 +0530

    model created for content handling

---
+++ identity-service/models/content.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const ContentSchema = new Schema(
+  {
+    contentName: {
+      type: String,
+      required: true,
+    },
+    contentDescription: {
+      type: String,
+    },
+    type: {
+      type: String,
+      enum: ["video", "reading"],
+    },
+    body: {
+      type: String,
+    },
+    source: {
+      type: String,
+    },
+    courseId: { type: Schema.Types.ObjectId, ref: "Course" },
+  },
+  { timestamps: true }
+);
+
+export default model("Content", ContentSchema);

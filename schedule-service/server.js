2399851aed08f4d8723fdf484e25b7c84bcc41af
Vikum Manchanayake <vikumviraj@vizuamatix.com>
2025-04-12 09:53:03 +0530

    add Schedule routes

--- schedule-service/server.js
+++ schedule-service/server.js
@@ -21,34 +21,33 @@ const BASE_URL = process.env.API_BASE_URL || "/api/v1";
 
 connectMongoDb();
 
-const swaggerDocs = swaggerJsDoc(getSwaggerOptions(PORT, BASE_URL));
-app.use(`${BASE_URL}/docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+// const swaggerDocs = swaggerJsDoc(getSwaggerOptions(PORT, BASE_URL));
+// app.use(`${BASE_URL}/docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+// app.options("*", cors(corsOptions));
 
 
 // Endpoints
 app.use(`${BASE_URL}/schedule`, scheduleRoutes);
 
-app.all("*", (req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({ message: "404 Not Found" });
-  } else {
-    res.type("txt").send("404 Not Fund");
-  }
-});
+// app.all("*", (req, res) => {
+//   res.status(404);
+//   if (req.accepts("html")) {
+//     res.sendFile(path.join(__dirname, "views", "404.html"));
+//   } else if (req.accepts("json")) {
+//     res.json({ message: "404 Not Found" });
+//   } else {
+//     res.type("txt").send("404 Not Fund");
+//   }
+// });
 
 // Error handling middleware
 app.use(errorMiddleware);
 
 // Server setup
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`API base URL: ${BASE_URL}`);

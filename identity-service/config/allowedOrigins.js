8886601ca582502e6d2bf884f081850254349244
anojnuclei <anoj@nuclei.tech>
2025-04-20 16:09:12 +0530

    add allowed origins

--- identity-service/config/allowedOrigins.js
+++ identity-service/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
   "http://localhost:3000",
+  "http://52.136.38.68:5000/",
   "http://localhost:5173",
   "https://yourproductiondomain.com",
   "https://www.yourproductiondomain.com",
